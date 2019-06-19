f4d6a3ade3fcd2845866e43ce9fb9317625e9526
unknown <31121986g@gmail.com>
2019-06-19 16:05:17 +0530

    learnig routing options

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,22 +12,29 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'servers', component: ServersComponent,
+  { path: '', component: HomeComponent  },
+
+  { path: 'servers', component: ServersComponent,
     children: 
     [
-      {path: ':id', component: ServerComponent},
-      {path: ':id/edit', component: EditServerComponent}  
-    ]
-  },  
-  {path: 'users', component: UsersComponent,
-  children: 
-    [
-      {path: ':id/:name', component: UserComponent}
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent  }  
     ]
   },
+
+  { path: 'users', component: UsersComponent,
+    children: 
+      [
+        { path: ':id/:name', component: UserComponent }
+      ]
+  },
+
+  { path: 'not-found', component: PageNotFoundComponent},
+
+  { path: '**', redirectTo: '/not-found'}
   
 ];
 
@@ -41,7 +48,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
